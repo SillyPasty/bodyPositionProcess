115720ccc8ab09b740b36dd9bd8c6577480a20b8
HD-shuo <1755656461@qq.com>
2019-12-11 16:29:50 +0800

    增加手部的判断

--- sitUp.py
+++ sitUp.py
@@ -19,10 +19,12 @@ def sitUpAnalysis(folder_path):
     l_waist_angle_list = []
     r_s_knee_angle_list = []
     l_s_knee_angle_list = []
-    #r_shouldertowrist_distance_list = []
-    #l_shouldertowrist_distance_list = []
+    r_elbowtoneck_dist_list = []
+    l_elbowtoneck_dist_list = []
 
-    cnt = 0
+    total_r_elbowtoneck_dist = 0
+    total_l_elbowtoneck_dist = 0
+    cnt = cnt_1 = cnt_2 = 0
 
     for i, file in enumerate(files):
         coor = jp.getJson(folder_path + '\\' + file)
@@ -33,8 +35,8 @@ def sitUpAnalysis(folder_path):
         l_waist_angle = getValue.getWaistAngle(coor,'L')
         r_s_knee_angle = getValue.getKneeAngle(coor,'R')
         l_s_knee_angle = getValue.getKneeAngle(coor,'L')
-        #l_shouldertowrist_distance = getValue.getgetShoulderToWristDistance(coor, 'L')
-        #r_shouldertowrist_distance = getValue.getgetShoulderToWristDistance(coor, 'R')
+        r_elbowtoneck_dist = getValue.getElbowToNeckDistance(coor,'R')
+        l_elbowtoneck_dist = getValue.getElbowToNeckDistance(coor,'L')
         tick.append(r_waist_angle)
 
         if r_waist_angle:
@@ -45,10 +47,17 @@ def sitUpAnalysis(folder_path):
             r_s_knee_angle_list.append(r_s_knee_angle)
         if l_s_knee_angle:
             l_s_knee_angle_list.append(l_s_knee_angle)
-        #if l_shouldertowrist_distance:
-            #l_shouldertowrist_distance_list.append(l_shouldertowrist_distance)
-        #if r_shouldertowrist_distance:
-            #r_shouldertowrist_distance_list.append(r_shouldertowrist_distance)
+        if r_elbowtoneck_dist:
+            r_elbowtoneck_dist_list.append(r_elbowtoneck_dist)
+            cnt_1 += 1
+            total_r_elbowtoneck_dist += r_elbowtoneck_dist
+        aver_r_elbowtoneck_dist = total_r_elbowtoneck_dist/cnt_1
+        if l_elbowtoneck_dist:
+            l_elbowtoneck_dist_list.append(l_elbowtoneck_dist)
+            cnt_2 += 1
+            total_l_elbowtoneck_dist += l_elbowtoneck_dist
+        aver_l_elbowtoneck_dist = total_l_elbowtoneck_dist/cnt_2
+
 
         if len(tick) == 5:
             tend = analysis.getTendency(tick, 8)  # One tick
@@ -60,15 +69,14 @@ def sitUpAnalysis(folder_path):
                         if tendency[-2] == 'upper':  # a period and tendency[-3] == 'upper'
                             cnt += 1
                             result['Num'] = cnt
-                            standard = analysis.SitUpPeriodJudge(r_waist_angle_list, l_waist_angle_list,r_s_knee_angle_list,l_s_knee_angle_list)
-                            #r_shouldertowrist_distance_list,l_shouldertowrist_distance)
+                            standard = analysis.SitUpPeriodJudge(r_waist_angle_list,l_waist_angle_list,r_s_knee_angle_list,l_s_knee_angle_list,r_elbowtoneck_dist_list,l_elbowtoneck_dist_list,aver_r_elbowtoneck_dist,aver_l_elbowtoneck_dist)
                             result['IsRWaistStandard'], result['IsLWaistStandard'] = standard
                             result['IsRKneeAngle'],result['IsLKneeAngle'] = standard
-                            #result['IsShouldertowrist'],result['IsShoudertowrist'] = standard
+                            result['IsRElbowtoNeckStandard'],result['IsLElbowtoNeckStandard'] = standard
                             result['Flag'] = i
                             r_waist_angle_list = l_waist_angle_list = []
-                            r_knee_angle_list = l_knee_angle_list = []
-                            r_shouldertowrist_distance_list = l_shouldertowrist_distance_list = []#序列置空
+                            r_s_knee_angle_list = l_s_knee_angle_list = []
+                            r_elbowtoneck_dist_list = l_elbowtoneck_dist_list = []#序列置空
                             results.append(result)
                             result = {}
 
