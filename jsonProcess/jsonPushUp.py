22f4e119d94516acdb8a06bfc30815e30aa140ac
王宇博 <2018213108@bupt.edu.cn>
2020-04-08 17:03:04 +0800

    添加视频流读取功能

--- jsonProcess/jsonPushUp.py
+++ jsonProcess/jsonPushUp.py
@@ -1,6 +1,7 @@
 from jsonProcess.TJsonProcess import TJsonProcess
 # import PySide2.QtCore as QtCore
 from datetime import datetime
+import cv2
 
 class JsonPushUp(TJsonProcess):
 
@@ -11,12 +12,18 @@ class JsonPushUp(TJsonProcess):
         print(types)
         startTime = datetime.now()
         length = 0
+        vStream = cv2.VideoCapture(0)
         while length < period:
+            _, frame = vStream.read()
+            cv2.imshow('ex1', frame)
             curTime = datetime.now()
             delta = curTime - startTime
             if delta.seconds != length:
                 length = delta.seconds
-                self.hisInfo.add(id=length, flag=1, stuno=1, type='situp', mark=1,
-                                num=1, time=datetime.now(), waist=1, arm=1,
-                                elbow=1, chin=1, flag2=0)
+                # self.hisInfo.add(id=length, flag=1, stuno=1, type='situp', mark=1,
+                #                 num=1, time=datetime.now(), waist=1, arm=1,
+                #                 elbow=1, chin=1, flag2=0)
                 self.signal_update_time.emit(period - length)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
